38738ae8cc12b4857b28c3c043371b58bd592e01
PratyushaGhosh03 <pratyushaghoshtildanga@gmail.com>
2025-02-17 22:03:37 +0530

    Create checkPrimeOrNot.py

---
+++ checkPrimeOrNot.py
@@ -0,0 +1,10 @@
+#Prime numbers
+n=int(input('Enter a number:'))
+count=0
+for i in range (1,n+1):
+  if(n%i == 0):
+    count=count+1
+if(count==2):
+  print(n,'is prime')
+else:
+  print(n,'is not prime')
